00b61ab1dd278c7f1d10cdd39f21f11829419731
maroco <luigi1215@naver.com>
2024-12-27 13:01:20 +0900

    fix : sentiment_predict의 모델 부분 수정 - 하드코딩에서 모듈 import로 변경

--- scripts/sentiment_predict.py
+++ scripts/sentiment_predict.py
@@ -2,30 +2,7 @@ import torch
 from transformers import BertTokenizer, BertModel
 from torch import nn
 import torch.nn.functional as F
-
-# KoBERT 모델 정의
-class BERTClassifier(nn.Module):
-    def __init__(self, bert):
-        super(BERTClassifier, self).__init__()
-        self.bert = bert  # Pre-trained BERT 모델
-        self.classifier = nn.Sequential(
-            nn.Linear(bert.config.hidden_size, 256),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(128, 2)  # 이진 분류
-        )
-
-    def forward(self, input_ids, attention_mask, token_type_ids):
-        outputs = self.bert(
-            input_ids=input_ids,
-            attention_mask=attention_mask,
-            token_type_ids=token_type_ids
-        )
-        pooled_output = outputs.pooler_output  # [CLS] 토큰의 임베딩
-        return self.classifier(pooled_output)
+from scripts.model import BERTClassifier
 
 # 감성 추론 함수
 def predict_sentiment(text, model, tokenizer, max_len=128):
